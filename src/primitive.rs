bf1b5326461b104403a0cb04abfb65aa0718f724
Nathan Stocks <nathan.stocks@gmail.com>
2017-07-07 20:16:40 -0600

    Working

--- src/primitive.rs
+++ src/primitive.rs
@@ -23,16 +23,6 @@ pub struct Coord {
     pub row : u16,
 }
 
-impl Coord {
-    pub fn at(col : u16, row : u16) -> Self {
-        Self {
-            col: col,
-            row: row,
-        }
-    }
-}
-
-
 pub fn clamp(x : u16, min : u16, max : u16) -> u16 {
     if x < min { min } else if x > max { max } else { x }
 }
